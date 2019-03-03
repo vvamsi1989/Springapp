a227646d587ec6bd4ae38952e52edab885dd7af1
Vamsikrishna Vuyyuru <vamsi@VamsikrhnasAir3.home>
2019-03-03 10:08:17 -0500

    Project Object Validation

--- Full_Stack_SpringBoot/src/main/java/com/fullstack/webapp/domain/Project.java
+++ Full_Stack_SpringBoot/src/main/java/com/fullstack/webapp/domain/Project.java
@@ -1,11 +1,14 @@
 package com.fullstack.webapp.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 
 @Entity
@@ -14,12 +17,21 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project Name should not be empty")
     private String projectName;
+    @NotBlank(message = "Project Identifier should not be blank")
+    @Size(min=4,max=5,message="Please give only 4 to 5 characters")
+    @Column(updatable = false,unique=true)
     private String projectIdentifier;
+    @NotBlank(message="description cannot be blank")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date update_At;
 
     public Project(){
