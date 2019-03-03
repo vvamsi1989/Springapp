7c71fc6c9cf06ff82c5b28a805ebde7eb384fe62
Vamsikrishna Vuyyuru <vamsi@VamsikrhnasAir3.home>
2019-03-03 14:00:28 -0500

    Validation

--- Full_Stack_SpringBoot/src/main/java/com/fullstack/webapp/web/ProjectController.java
+++ Full_Stack_SpringBoot/src/main/java/com/fullstack/webapp/web/ProjectController.java
@@ -2,6 +2,7 @@ package com.fullstack.webapp.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.fullstack.webapp.services.ProjectService;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
@@ -21,7 +24,10 @@ public class ProjectController {
 
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+        if(result.hasErrors()){
+            return new ResponseEntity<String>("Invalid Project Object",HttpStatus.BAD_REQUEST);
+        }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
