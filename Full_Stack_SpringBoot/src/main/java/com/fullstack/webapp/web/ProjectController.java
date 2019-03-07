8ddc1d0a89a78818200a1889614370e952162d34
Vamsikrishna Vuyyuru <vamsi@VamsikrhnasAir3.home>
2019-03-07 17:30:46 -0500

    Refactor Controller -Validation

--- Full_Stack_SpringBoot/src/main/java/com/fullstack/webapp/web/ProjectController.java
+++ Full_Stack_SpringBoot/src/main/java/com/fullstack/webapp/web/ProjectController.java
@@ -1,5 +1,6 @@
 package com.fullstack.webapp.web;
 
+import com.fullstack.webapp.services.ValidationErrorSerivce;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
@@ -25,17 +26,12 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
-
+    @Autowired
+    private ValidationErrorSerivce mapValidationErrorService;
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
-        if(result.hasErrors()){
-
-            Map<String,String> errorMap = new HashMap();
-            for(FieldError error:result.getFieldErrors()){
-                errorMap.put(error.getField(),error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String,String>>(errorMap,HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap=mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
